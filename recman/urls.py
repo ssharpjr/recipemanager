754962e8dbd24861479781f34560996d1e8c375d
Stacey Sharp <ssharpjr@gmail.com>
2015-10-05 23:14:24 -0400

    Updating CSS and forms

--- recman/urls.py
+++ recman/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.recipe_list, name='recipe_list'),
+	url(r'^recipe/(?P<pk>[0-9]+)/$', views.recipe_detail, name='recipe_detail'),
+	url(r'^recipe/new/$', views.recipe_new, name='recipe_new'),
 ]
--- recman/views.py
+++ recman/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Recipe
+from .models import Recipe, Recipe_detail
+
+from .forms import RecipeForm
 
 
 def recipe_list(request):
 	recipes = Recipe.objects.all().order_by('name')
 	return render(request, 'recman/recipe_list.html', {'recipes': recipes})
 
+def recipe_detail(request, pk):
+	recipe = get_object_or_404(Recipe, pk=pk)
+	return render(request, 'recman/recipe_detail.html', {'recipe': recipe})
+
+def recipe_new(request):
+	form = RecipeForm()
+	return render(request, 'recman/recipe_edit.html', {'form': form})
